602775bb32f8ed18757e51b3139deb2fd81a0349
rugieri <rugieri.ronaldo@gmail.com>
2021-10-28 22:04:44 -0300

    updating state properties

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+
     window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+          lon: position.coords.longitude,
+        });
+      },
       (err) => console.log(err)
     );
-    return <div>Hello !!!!</div>;
+  }
+
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Longitude: {this.state.lon}
+      </div>
+    );
   }
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
